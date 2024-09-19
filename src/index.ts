a90ee1e417546559b49eb53976da59c29f221055
Kevin Mejia <54713858+nivekino@users.noreply.github.com>
2024-09-18 20:42:17 -0600

    remove limiter

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
-import rateLimit from "express-rate-limit";
 import { json } from "body-parser";
 import creditRoutes from "./routes/credit.routes";
 import logger from "./utils/logger";
@@ -31,15 +30,6 @@ try {
   app.use(helmet());
   app.use(morgan("dev"));
   app.use(json());
-
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message:
-      "Too many requests from this IP, please try again after 15 minutes",
-  });
-
-  app.use(limiter);
 
   app.get("/", (req, res) => {
     res.status(200).json({ msg: "Server is up and running" });
